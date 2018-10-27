4976a659e64cfb885894e299340394ad99dc51e3
Steven Hanson <essayhanson@gmail.com>
2018-10-27 14:15:03 +0100

    App renamed and bootstrapped

---
+++ musicianquiz/src/MusicianQuiz.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './bootstrap.min.css';
+
+class MusicianQuiz extends Component {
+  render() {
+    return (
+      <div>Musician Quiz</div>
+    );
+  }
+}
+
+export default MusicianQuiz;
