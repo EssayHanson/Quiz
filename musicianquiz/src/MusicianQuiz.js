a2536264d8ee87ffc68727507fe73e8a878aa555
Steven Hanson <essayhanson@gmail.com>
2018-10-27 14:38:56 +0100

    imports added to MusicianQuiz js

--- musicianquiz/src/MusicianQuiz.js
+++ musicianquiz/src/MusicianQuiz.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import './bootstrap.min.css';
+import Hero from './components/Hero';
+import Turn from './components/Turn';
+import Continue from './components/Continue';
 
 class MusicianQuiz extends Component {
   render() {
     return (
-      <div>Musician Quiz</div>
+      <div className="container-fluid">
+        <Hero/>
+        <Turn/>
+        <Continue/>
+      </div>
     );
   }
 }
