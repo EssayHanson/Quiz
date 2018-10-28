3c340b43d895262f1a824e3aff4fede58ce60a2f
Steven Hanson <essayhanson@gmail.com>
2018-10-28 16:29:20 +0000

    shuffle functionality added

--- musicianquiz/src/index.js
+++ musicianquiz/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import MusicianQuiz from './MusicianQuiz';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
 
 const artists = [
     {
@@ -11,24 +12,43 @@ const artists = [
         imageSource: 'Wikipedia',
         albums: [
             'My Love is your Love',
-            '21',
-            'The Gold Collection'
+            'I Look to You',
+            'I Will Always Love You'
         ]
     },
     {
         name: 'Amy Winehouse', 
         imageUrl: '/images/amy.jpg',
         imageSource: 'Wikipedia',
-        albums: ['Back to Black']
+        albums: [
+            'Back to Black', 
+            'Frank', 
+            'Lioness'
+        ]
     }
 ];
 
+function getTurnData(artists) {
+
+    const allAlbums = artists.reduce(function(p, c, i) {
+        return p.concat(c.albums);
+    }, []);
+
+    const fourRandomAlbums = shuffle(allAlbums).slice(0,4);
+    const answer = sample(fourRandomAlbums);
+
+    return {
+
+        albums: fourRandomAlbums,
+        artist: artists.find((artist) => 
+            artist.albums.some((title) =>
+                title === answer))
+    }
+}
+
 const state = {
 
-    turnData: {
-        artist: artists[0],
-        albums: artists[0].albums
-    }
+    turnData: getTurnData(artists) 
 };
 
 ReactDOM.render(<MusicianQuiz {...state} />, document.getElementById('root'));
