9d0141e678210d674676aa8aeaa6b5412a88ba65
Steven Hanson <essayhanson@gmail.com>
2018-10-28 15:08:55 +0000

    key added

--- musicianquiz/src/components/Turn.js
+++ musicianquiz/src/components/Turn.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
-const Turn = () => {
+function Turn({artist, albums, i}) {
 
-    return(<div/>)
+    return(<div className="row turn" style={{backgroundColor: "white"}}>
+        <div className="col-4 offset-1">
+            <img src={artist.imageUrl} className="artistImage" alt="Artist"/>
+        </div>
+        <div className="col-6">
+            {albums.map((title, i) => <p key={i}>{title}</p>)}
+        </div>
+    </div>);
 }
 
 export default Turn;
