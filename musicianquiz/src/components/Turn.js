02bae9a92c90feb25dde0b0a4378c1341de70e81
Steven Hanson <essayhanson@gmail.com>
2018-10-28 16:06:59 +0000

    new component link

--- musicianquiz/src/components/Turn.js
+++ musicianquiz/src/components/Turn.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import Album from '../components/Album';
 
-function Turn({artist, albums, i}) {
+function Turn({artist, albums}) {
 
     return(<div className="row turn" style={{backgroundColor: "white"}}>
         <div className="col-4 offset-1">
             <img src={artist.imageUrl} className="artistImage" alt="Artist"/>
         </div>
         <div className="col-6">
-            {albums.map((title, i) => <p key={i}>{title}</p>)}
+            {albums.map((title, i) => <Album key={i} title={title}/>)}
         </div>
     </div>);
 }
